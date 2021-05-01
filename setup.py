db398310fd1e5229910e20647f8c2ce172bfe3f4
kurama8103 <38579582+kurama8103@users.noreply.github.com>
2021-05-01 13:23:53 +0900

    revise

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kurama8103/tdnet_tool",
     packages=setuptools.find_packages(),
-    install_requires=["pandas", "bs4","time"],
+    install_requires=["pandas", "bs4"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
